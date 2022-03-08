16e98265c3c496ff8ce19a270cf412f69b2ab319
Hai Vo <haivotrung@gmail.com>
2022-03-07 21:38:48 -0500

    added apdding to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header(){
     return (
-        <nav className="bg-[#1A1B21]">
+        <nav className="bg-[#1A1B21] p-5">
             <img className="rounded-lg w-fit h-auto sm:w-1/3 sm:h-auto md:w-1/3 md:h-auto mx-auto" src={require('./styles/Avatar Hai Vo.jpeg')} alt="Profile" />
             <div className="flex flex-col justify-center items-center">
                 <h1 className="pt-5 font-bold text-4xl text-white">Hai Vo</h1>
