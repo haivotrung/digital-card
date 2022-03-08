3d437340e26d0a710002260209c8fd6ec3a5529a
Hai Vo <haivotrung@gmail.com>
2022-03-07 20:47:55 -0500

    updated tailwind css and added info

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function Header(){
     return (
-        <nav className="flex flex-col justify-center items-center bg-[#1A1B21]">
-            <img className="max-w-md" src={require('./styles/Avatar.png')} alt="Profile" />
-            <h1 className="pt-5 font-bold text-4xl text-white">Laura Smith</h1>
-            <p className="p-2 font-regular text-l text-[#F3BF99]">Frontend Developer</p>
-            <p className="text-white">laurasmith.website</p>
+        <nav className="bg-[#1A1B21]">
+            <img className="rounded-lg w-fit h-auto md:w-60 md:h-auto mx-auto" src={require('./styles/Avatar Hai Vo.jpeg')} alt="Profile" />
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="pt-5 font-bold text-4xl text-white">Hai Vo</h1>
+                <p className="p-2 font-regular text-l text-[#F3BF99]">Frontend Developer</p>
+                <a href="mailto: hai@ediciones.nyc" className="text-white">hai@ediciones.nyc</a>
+            </div>
         </nav>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,34 @@
 import React from "react";
 import {ReactComponent as EmailLogo} from "./styles/icons/email.svg"
-import {ReactComponent as LinkedInLogo} from "./styles/icons/linkedin.svg"
+import { ReactComponent as LinkedInLogo } from "./styles/icons/linkedin.svg"
+import {ReactComponent as GitHubLogo} from "./styles/icons/github.svg"
+
 
 export default function Main(){
     return (
         <main className="bg-[#1A1B21]">
         
-            <div className="flex flex-row justify-center items-centers content-center pt-5 space-x-4">
-                <button class="inline-flex items-center h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
-                <EmailLogo />
-                <span className="px-6">Email</span>
+            <div className="flex flex-row justify-center items-centers content-center pt-5 space-x-5">
+                <button class="inline-flex items-center h-10 px-5 text-[#1E1F26] rounded-lg transition ease-in-out bg-white hover:-translate-y-0.5 hover:scale-105	hover:bg-slate-400 duration-300" >
+                <GitHubLogo />
+                <a href="https://github.com/haivotrung" className="px-4">GitHub</a>
                 </button>
 
-                <button class="inline-flex items-center h-10 px-5 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+                <button class="inline-flex items-center h-10 px-5 text-white bg-[#5093E2] rounded-lg transition ease-in-out hover:-translate-y-0.5 hover:scale-105 hover:bg-blue-800 duration-300">
                 <LinkedInLogo />    
-                <span className="px-4">LinkedIn</span>
+                <a href="https://www.linkedin.com/in/hai-t-vo/" className="px-4">LinkedIn</a>
                 </button>
-
             </div>
+
 
             <div className="p-10 text-white m-auto max-w-xl">
             <h3 className="font-bold text-2xl">About</h3>
-            <p className="font-normal pt-2">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p className="font-normal pt-2">problem-solving. everything that involves the technological side of the digital realm is his area of expertise. hai is always up for freelancing and consulting on additional projects specifically working with other queer and trans people of color.</p>
             
-                <br></br>
+            <br></br>
                 
             <h3 className="font-bold text-2xl">Interests</h3>
-            <p className="font-normal pt-2">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+            <p className="font-normal pt-2">a pop music aficionado, coffee enthusiast, and moonlighting in learning more about vino.</p>
             </div>
 
         </main>
